e73614d57ad56b2133d5793fbdeb498688770521
Jan Range <range.jan@web.de>
2024-12-27 22:49:06 +0100

    ignore for now

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
       },
       // Enable esbuild polyfill plugins
       plugins: [
+        // @ts-ignore
         NodeGlobalsPolyfillPlugin({
           process: true,
           buffer: true,
