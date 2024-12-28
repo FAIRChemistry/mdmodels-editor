bc21731cd19e4b539e1a8ae1c0e79788663d277c
Jan Range <range.jan@web.de>
2024-12-28 14:08:01 +0100

    move initial model to separate file

---
+++ src/content/initial-model.md
@@ -0,0 +1,44 @@
+# MD-Models 
+
+Welcome to the MD-Models editor! 
+
+## Getting Started
+
+- Unfamiliar with MD-Models? To learn more about MD-Models, please click on the question mark in the top right corner.
+- To import a model from a GitHub repository, click on the "GitHub" button in the top right corner.
+- To export your model to a file, click on the "Download" button in the top right corner.
+- Left to the editor, you can see the table of contents of your model. Click on an item to navigate to it.
+- Right to the editor, you can see validation errors and warnings.
+- Try out the graph editor by clicking on the "Graph" button in the top right corner.
+
+Looking for examples? Please click on the "GitHub" button in the top right corner and choose one of the examples.
+
+To get started, simply edit this text and start writing your own model!
+
+### Person
+
+This is a place to add description of the object.
+
+- __name__
+  - Type: string
+  - Term: schema:name
+  - Description: The name of the person.
+
+- age
+  - Type: integer
+  - Term: schema:age
+  - Description: The age of the person.
+
+- address
+  - Type: Address
+  - Term: schema:address
+  - Description: The address of the person.
+
+### Address
+
+This is a place to add description of the object.
+
+- street
+  - Type: string
+  - Term: schema:street
+  - Description: The street of the address.
--- src/lib/stores/validator-store.ts
+++ src/lib/stores/validator-store.ts
@@ -4,52 +4,7 @@ import { getErrors } from "../validation";
 import { Tab, ValidationError } from "@/types";
 import { getMdModelStructure } from "../mdutils";
 import { DataModel } from "mdmodels-core";
-
-export const INITIAL_CODE = `# MD-Models 
-
-Welcome to the MD-Models editor! 
-
-## Getting Started
-
-- Unfamiliar with MD-Models? To learn more about MD-Models, please click on the question mark in the top right corner.
-- To import a model from a GitHub repository, click on the "GitHub" button in the top right corner.
-- To export your model to a file, click on the "Download" button in the top right corner.
-- Left to the editor, you can see the table of contents of your model. Click on an item to navigate to it.
-- Right to the editor, you can see validation errors and warnings.
-- Try out the graph editor by clicking on the "Graph" button in the top right corner.
-
-Looking for examples? Please click on the "GitHub" button in the top right corner and choose one of the examples.
-
-To get started, simply edit this text and start writing your own model!
-
-### Person
-
-This is a place to add description of the object.
-
-- __name__
-  - Type: string
-  - Term: schema:name
-  - Description: The name of the person.
-
-- age
-  - Type: integer
-  - Term: schema:age
-  - Description: The age of the person.
-
-- address
-  - Type: Address
-  - Term: schema:address
-  - Description: The address of the person.
-
-### Address
-
-This is a place to add description of the object.
-
-- street
-  - Type: string
-  - Term: schema:street
-  - Description: The street of the address.
-`;
+import INITIAL_CODE from "@/content/initial-model.md?raw";
 
 /**
  * Store interface for managing validation errors
@@ -97,35 +52,6 @@ export const useValidatorStore = create<ValidatorStore>()(
       getStructure: () => get().structure,
     }))
   )
-);
-
-// Monitor store updates
-useValidatorStore.subscribe(
-  (state) => state.code,
-  (code) => {
-    console.log("Code updated:", code.length);
-  }
-);
-
-useValidatorStore.subscribe(
-  (state) => state.errors,
-  (errors) => {
-    console.log("Errors updated:", errors.length);
-  }
-);
-
-useValidatorStore.subscribe(
-  (state) => state.structure,
-  (structure) => {
-    console.log("Structure updated:", structure ? "present" : "null");
-  }
-);
-
-useValidatorStore.subscribe(
-  (state) => state.selectedTab,
-  (tab) => {
-    console.log("Selected tab updated:", tab);
-  }
 );
 
 export const useCode = () => useValidatorStore((state) => state.code);
